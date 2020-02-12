edae3013f38c43a1bca1b4340dad616451e81132
Alex <Magana@Alexs-MacBook-Air.local>
2020-02-11 22:39:20 -0600

    links working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    
     <Navigation />
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/contact" exact component={Contact} />
   </div>
 );
 
